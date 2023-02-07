b79dd9cf0d21108c1c84fc14c140893f548c74a1
Nguyễn Thanh Đông <thanhdonghcmute@gmail.com>
2023-02-07 22:39:29 +0700

    101. Xây dựng UI phần Header #5

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -26,6 +26,21 @@ const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
         title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    type: 'other',
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -46,6 +61,16 @@ function Header() {
             setSearchResults([]);
         }, 0);
     }, []);
+
+    // Handle logic
+    const HandleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'language':
+                // Handle change language
+                break;
+            default:
+        }
+    };
 
     return (
         <header className={cx('wrapper')}>
@@ -81,7 +106,7 @@ function Header() {
                     {/* <Button text>Upload</Button> */}
                     <UploadButton></UploadButton>
                     <Button primary>Log in</Button>
-                    <Menu items={MENU_ITEMS}>
+                    <Menu items={MENU_ITEMS} onChange={HandleMenuChange}>
                         <button className={cx('more-btn')}>
                             <FontAwesomeIcon icon={faEllipsisVertical}></FontAwesomeIcon>
                         </button>
